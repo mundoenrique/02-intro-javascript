cf96cc7ee80160305bdcb3503412c7cb135c7fa3
mundoenrique <mundoenrique@hotmail.com>
2022-06-17 14:09:31 -0400

    Let y constantes

---
+++ src/bases/LetAndConst.js
@@ -0,0 +1,24 @@
+function LetAndConst() {
+	const nombre = 'Enrique';
+	const apellido = 'Peñaloza';
+	let valorDado = 5;
+	valorDado = 6;
+
+	const scopeVar = () => {
+		const nombre = 'Yayita';
+
+		return nombre;
+	};
+
+	return (
+		<>
+			<h2>Varibles y constantes</h2>
+			<p>Nombre: "{nombre}"</p>
+			<p>Apellido: "{apellido}"</p>
+			<p>valorDado: "{valorDado}"</p>
+			<p>Variable de scope "{scopeVar()}"</p>
+		</>
+	);
+}
+
+export default LetAndConst;
