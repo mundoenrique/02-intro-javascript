0c7c6a4e6508aa89ee3e36f771baa4624542811b
mundoenrique <mundoenrique@hotmail.com>
2022-06-21 09:45:31 -0400

    Functions />

--- src/bases/05-Functions.js
+++ src/bases/05-Functions.js
@@ -1,58 +1,58 @@
+// Función nombrada
+function greeting1(nombre) {
+	return `Hola ${nombre}`;
+}
+
+// Función nombrada en constante
+const greeting2 = function (nombre) {
+	return `Hola ${nombre}`;
+};
+
+// Fucnción de flecha con return explicito
+const greeting3 = (nombre) => {
+	return `Hola ${nombre}`;
+};
+
+// Fucnción de flecha con return implicito
+// Si solo recibe un parametro, se puede omitir los parentesis
+// Si solo tiene una linea de codigo, se puede omitir los llaves y el return
+const greeting4 = (nombre) => `Hola ${nombre}`;
+
+// Función de flecha con return explicito sin parametros
+const getUser1 = () => {
+	return {
+		uid: 'ABC123',
+		userName: 'mundoenrique',
+	};
+};
+
+// Función de flecha con return implicito sin parametros
+// Si returna un objeto se debe encerrar en parentesis el objeto
+export const getUser2 = () => ({
+	uid: 'ABC124',
+	userName: 'yayita',
+});
+
+// Función de flecha con return implicito con un parametro
+// Si solo recibe un parametro, se puede omitir los parentesis
+// Para devolver un atributo con el mismo nombre de la variabble, solo escribir el nombre de la variable
+export const activeUser = (user) => ({
+	uid: 'ABC124',
+	user,
+	userName: user,
+});
+
 export default function Functions() {
-	// Función nombrada
-	function saludar1(nombre) {
-		return `Hola ${nombre}`;
-	}
-
-	// Función nombrada en constante
-	const saludar2 = function (nombre) {
-		return `Hola ${nombre}`;
-	};
-
-	// Fucnción de flecha con return explicito
-	const saludar3 = (nombre) => {
-		return `Hola ${nombre}`;
-	};
-
-	// Fucnción de flecha con return implicito
-	// Si solo recibe un parametro, se puede omitir los parentesis
-	// Si solo tiene una linea de codigo, se puede omitir los llaves y el return
-	const saludar4 = (nombre) => `Hola ${nombre}`;
-
-	// Función de flecha con return explicito sin parametros
-	const usuario1 = () => {
-		return {
-			uid: 'ABC123',
-			userName: 'mundoenrique',
-		};
-	};
-
-	// Función de flecha con return implicito sin parametros
-	// Si returna un objeto se debe encerrar en parentesis el onjeto
-	const usuario2 = () => ({
-		uid: 'ABC124',
-		userName: 'yayita',
-	});
-
-	// Función de flecha con return implicito con un parametro
-	// Si solo recibe un parametro, se puede omitir los parentesis
-	// Para devolver un atributo con el mismo nombre de la variabble, solo escribir el nombre de la variable
-	const usuarioActivo = (user) => ({
-		uid: 'ABC124',
-		user,
-		userName: user,
-	});
-
 	return (
 		<>
 			<h2>Funciones</h2>
-			<p>Función saludar 1: {saludar1('Yajaira')}</p>
-			<p>Función saludar 2: {saludar2('Shamyr')}</p>
-			<p>Función saludar 3: {saludar3('Isabel')}</p>
-			<p>Función saludar 4: {saludar4('Ruth')}</p>
-			<p>Función usuario 1: {JSON.stringify(usuario1())}</p>
-			<p>Función usuario 2: {JSON.stringify(usuario2())}</p>
-			<p>Función usuario activo: {JSON.stringify(usuarioActivo('tere'))}</p>
+			<p>Función saludar 1: {greeting1('Yajaira')}</p>
+			<p>Función saludar 2: {greeting2('Shamyr')}</p>
+			<p>Función saludar 3: {greeting3('Isabel')}</p>
+			<p>Función saludar 4: {greeting4('Ruth')}</p>
+			<p>Función usuario 1: {JSON.stringify(getUser1())}</p>
+			<p>Función usuario 2: {JSON.stringify(getUser2())}</p>
+			<p>Función usuario activo: {JSON.stringify(activeUser('tere'))}</p>
 		</>
 	);
 }
---
+++ src/tests/05-Functions.test.js
@@ -0,0 +1,28 @@
+import { activeUser, getUser2 } from '../bases/05-Functions';
+
+describe('Prueba en <Functions />', () => {
+	test('getUser2 debe retornar un objeto', () => {
+		const testUser = {
+			uid: 'ABC124',
+			userName: 'yayita',
+		};
+
+		const user = getUser2();
+
+		expect(user).toEqual(testUser);
+	});
+
+	test('activeUser debe retornar un objeto', () => {
+		const name = 'tere';
+
+		const testUser2 = {
+			uid: 'ABC124',
+			user: name,
+			userName: name,
+		};
+
+		const user = activeUser(name);
+
+		expect(user).toStrictEqual(testUser2);
+	});
+});
